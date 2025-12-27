4c75f2cb7205f0e262da6f04297276e664d5614f
Peter Ritchie <peteraritchie@users.noreply.github.com>
2025-12-27 11:04:32 -0500

    chore: Code cleanup

--- src/DotnetPsCmds/AddDotnetPackageCmdlet.cs
+++ src/DotnetPsCmds/AddDotnetPackageCmdlet.cs
@@ -43,10 +43,14 @@ public class AddDotnetPackageCmdlet : PSCmdlet
 	public string? PackageVersion { get; set; }
 
 	/// <summary>
-	/// Gets or sets a value indicating whether prerelease packages are allowed to be installed.
+	/// Gets or sets a value indicating whether prerelease packages
+	/// are allowed to be installed.
 	/// </summary>
-	/// <remarks>If set to <see langword="true"/>, prerelease versions of packages may be included during
-	/// installation. If <see langword="false"/>, only stable package versions are considered. If <see langword="null"/>,
+	/// <remarks>If set to <see langword="true"/>, prerelease versions of
+	/// packages may be included during installation.
+	/// If <see langword="false"/>, only stable package versions are
+	/// considered.
+	/// If <see langword="null"/>,
 	/// the default behavior is used.</remarks>
 	[Parameter(Mandatory = false,
 		Position = 3,
@@ -71,7 +75,11 @@ public class AddDotnetPackageCmdlet : PSCmdlet
 			throw new PSArgumentNullException(nameof(Project));
 		}
 		var executor = ShellExecutor.Instance;
-		var command = DetermineCommand(executor, Project!, PackageId!, PackageVersion, Prerelease ?? false);
+		var command = DetermineCommand(executor,
+			Project!,
+			PackageId!,
+			PackageVersion,
+			Prerelease ?? false);
 		if (ShouldProcess(command.Target, command.ActionName))
 		{
 			var r = command.Execute() as ShellOperationResult;
@@ -79,7 +87,10 @@ public class AddDotnetPackageCmdlet : PSCmdlet
 			WriteDebug($"Output:{Environment.NewLine}{r.OutputText}");
 			if (!string.IsNullOrWhiteSpace(r.ErrorText))
 			{
-				WriteError(new ErrorRecord(new Exception(r.ErrorText), "AddPackageFailed", ErrorCategory.NotSpecified, Project));
+				WriteError(new ErrorRecord(new Exception(r.ErrorText),
+					"AddPackageFailed",
+					ErrorCategory.NotSpecified,
+					Project));
 			}
 			else
 			{
@@ -88,11 +99,29 @@ public class AddDotnetPackageCmdlet : PSCmdlet
 		}
 	}
 
-	private CommandBase DetermineCommand(IShellExecutor executor, DotnetProject project, string packageId, string? packageVersion, bool isPrerelease)
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="executor"></param>
+	/// <param name="project"></param>
+	/// <param name="packageId"></param>
+	/// <param name="packageVersion"></param>
+	/// <param name="isPrerelease"></param>
+	/// <returns></returns>
+	private static CommandBase DetermineCommand(IShellExecutor executor,
+		DotnetProject project,
+		string packageId,
+		string? packageVersion,
+		bool isPrerelease)
 	{
 		var command = string.IsNullOrWhiteSpace(packageVersion)
-			? new AddPackageReferenceToProjectCommand(executor, project, packageId) { IsPrerelease = isPrerelease }
-			: new AddPackageReferenceToProjectCommand(executor, project, packageId, packageVersion!) { IsPrerelease = isPrerelease };
+			? new AddPackageReferenceToProjectCommand(executor,
+				project,
+				packageId) { IsPrerelease = isPrerelease }
+			: new AddPackageReferenceToProjectCommand(executor,
+				project,
+				packageId,
+				packageVersion!) { IsPrerelease = isPrerelease };
 		return command;
 	}
 }
