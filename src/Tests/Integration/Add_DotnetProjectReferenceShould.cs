863a4f16f4a29baec2a3324ce371071b5a4f9945
Peter Ritchie <peteraritchie@users.noreply.github.com>
2025-12-27 11:03:51 -0500

    chore: added integration test filter to CI/PR pipeline

---
+++ src/Tests/Integration/Add_DotnetProjectReferenceShould.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests.Integration;
+
+// ReSharper disable once InconsistentNaming
+public class Add_DotnetProjectReferenceShould
+{
+	[Fact]
+	[Trait("Category", "Integration")]
+	public void Work()
+	{
+		Assert.True(true);
+	}
+}
