e027087918e86192624ab1d3c522993986d178cc
Peter Ritchie <peteraritchie@users.noreply.github.com>
2025-12-31 16:21:46 -0500

    feat: added project configuration service. Fixes #36

---
+++ src/DotnetProjects/VisualStudioProjectConfigurationService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+using Tests;
+
+namespace Pri.Essentials.DotnetProjects;
+
+/// <summary>
+/// Provides services for configuring Visual Studio project files.
+/// </summary>
+public static class VisualStudioProjectConfigurationService
+{
+	/// <summary>
+	/// Adds the RestorePackagesWithLockFile property if it doesn't exist,
+	/// and sets it to true
+	/// </summary>
+	/// <param name="filePath"></param>
+	/// <exception cref="InvalidOperationException"></exception>
+	public static void SetRestorePackagesWithLockFile(string filePath)
+	{
+		using FileStream fs = new(filePath,
+			FileMode.Open,
+			FileAccess.ReadWrite,
+			FileShare.None);
+		SetRestorePackagesWithLockFile(fs);
+	}
+
+	private static void SetRestorePackagesWithLockFile(Stream fs)
+	{
+		var doc = XDocument.Load(fs);
+		var projectElement = doc.Root!;
+		if (projectElement.Name != ProjectElementNames.Project)
+		{
+			throw new InvalidOperationException(
+				"Root element must be <Project>.");
+		}
+
+		var propertyGroupElement
+			= projectElement.Element(ProjectElementNames.PropertyGroup) ??
+			  throw new InvalidOperationException(
+				  "No <PropertyGroup> element found in project file.");
+
+		if (TrySetProjectOption(propertyGroupElement,
+			    ProjectElementNames.RestorePackagesWithLockFile,
+			    true))
+		{
+			// Reset stream before saving
+			fs.SetLength(0);
+			fs.Position = 0;
+			doc.Save(fs);
+		}
+	}
+
+	private static bool TrySetProjectOption<T>(XElement propertyGroupElement,
+		string optionName,
+		T value)
+	{
+		var option = propertyGroupElement.Element(optionName);
+		if (option != null)
+		{
+			return false;
+		}
+
+		propertyGroupElement.Add(
+			new XElement(
+				optionName,
+				value?.ToString()));
+		return true;
+	}
+}
---
+++ src/Tests/VisualStudioProjectConfigurationServiceShould.cs
@@ -0,0 +1,77 @@
+﻿using System.Xml.Linq;
+using System.Xml.XPath;
+
+using Pri.Essentials.DotnetProjects;
+
+namespace Tests;
+
+public class VisualStudioProjectConfigurationServiceShould
+{
+	[Theory]
+	[InlineData("""
+	            <Project Sdk="Microsoft.NET.Sdk">
+	              <PropertyGroup>
+	                <TargetFramework>netstandard2.0</TargetFramework>
+	                <LangVersion>latest</LangVersion>
+	                <Nullable>enable</Nullable>
+	              </PropertyGroup>
+	            </Project>
+	            """)]
+	[InlineData("""
+	            <Project Sdk="Microsoft.NET.Sdk">
+	              <PropertyGroup>
+	                <TargetFramework>netstandard2.0</TargetFramework>
+	                <LangVersion>latest</LangVersion>
+	                <Nullable>enable</Nullable>
+	                <RestorePackagesWithLockFile>true</RestorePackagesWithLockFile>
+	              </PropertyGroup>
+	            </Project>
+	            """)]
+	void SetRestorePackagesWithLockFileSuccessfully(string xmlText)
+	{
+		string? filePath = null;
+		try
+		{
+			filePath = Path.GetTempFileName();
+			File.WriteAllText(filePath, xmlText);
+
+			VisualStudioProjectConfigurationService.SetRestorePackagesWithLockFile(filePath);
+
+			var doc = XDocument.Load(filePath);
+			var flag = doc.XPathSelectElement(
+				"/Project/PropertyGroup/RestorePackagesWithLockFile");
+			Assert.NotNull(flag);
+		}
+		finally
+		{
+			File.Delete(filePath!);
+		}
+	}
+
+	[Theory]
+	[InlineData("""
+	            <NotProject Sdk="Microsoft.NET.Sdk">
+	              <PropertyGroup>
+	                <TargetFramework>netstandard2.0</TargetFramework>
+	                <LangVersion>latest</LangVersion>
+	                <Nullable>enable</Nullable>
+	              </PropertyGroup>
+	            </NotProject>
+	            """)]
+	void ThrowWithBadProject(string xmlText)
+	{
+		string? filePath = null;
+		try
+		{
+			filePath = Path.GetTempFileName();
+			File.WriteAllText(filePath, xmlText);
+
+			Assert.Throws<InvalidOperationException>(() =>
+				VisualStudioProjectConfigurationService.SetRestorePackagesWithLockFile(filePath));
+		}
+		finally
+		{
+			File.Delete(filePath!);
+		}
+	}
+}
